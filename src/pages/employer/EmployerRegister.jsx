83de993c8d8adc8d37a754c15eb5b24d82a9dba5
Hilmi ŞAFAK <82928954+Hilmi51@users.noreply.github.com>
2022-04-22 19:14:23 +0300

    first commit

---
+++ src/pages/employer/EmployerRegister.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Button, Checkbox, Form, Container} from 'semantic-ui-react';
+import '../../App.css'
+
+
+export default function EmployerRegister() {
+
+}
---
+++ src/services/jobAdvertisementService.jsx
@@ -0,0 +1,24 @@
+import axios from "axios"
+
+export default class jobAdvertisementService {
+
+    getJobAdvertisements() {
+        return axios.get("http://localhost:8080/api/jobadvertisement/getall")
+    }
+
+    getByIsActive() {
+        return axios.get("http://localhost:8080/api/jobadvertisement/getByIsActive")
+    }
+
+    getByAppealDeadline() {
+        return axios.get("http://localhost:8080/api/jobadvertisement/getByAppealDeadline")
+    }
+
+    getByCompanyName(params) {
+        return axios.get(`http://localhost:8080/api/jobadvertisement/getAllOpenedPositionByCompanyName?employerId=${params}`)
+    }
+
+    getByIsActive() {
+        return axios.get("http://localhost:8080/api/jobadvertisement/getByIsActive")
+    }
+}
